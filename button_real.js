dcfd9dee57ed068f836205a1dd43f79442560dd0
Matheus Henrique A. de Santana <53881266+MatheusKoichi@users.noreply.github.com>
2021-05-06 17:23:53 -0300

    Update button_real.js

--- button_real.js
+++ button_real.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-    
+
     if (window.location.href = "https://claracloud.freshservice.com/support/catalog/items/55") {
 
         document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick =
@@ -21,5 +20,3 @@
         }
 
     }
-
-</script>
