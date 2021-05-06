7453f9854206e77a3ae61efa6e5a680169d7db82
Matheus Henrique A. de Santana <53881266+MatheusKoichi@users.noreply.github.com>
2021-05-06 18:37:25 -0300

    Update button_real.js

--- button_real.js
+++ button_real.js
@@ -1,5 +1,6 @@
 <script>
-
+if (window.location.href = "https://claracloud.freshservice.com/support/catalog/items/55") {
+    
     document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick = function setStartTime() {
         document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_time').value =
             new Date().toLocaleTimeString(navigator.language, {
@@ -23,4 +24,5 @@
         document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_date').value =
             new Date().toLocaleDateString();
     }
+}
 </script>
