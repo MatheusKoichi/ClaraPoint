c3938a557033563ada3201e37198f9457fb4bffa
Matheus Henrique A. de Santana <53881266+MatheusKoichi@users.noreply.github.com>
2021-05-06 19:20:23 -0300

    Update button_real.js

--- button_real.js
+++ button_real.js
@@ -1,6 +1,4 @@
-<script>
-    
-    document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick = function setStartTime() {
+document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick = function setStartTime() {
         document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_time').value =
             new Date().toLocaleTimeString(navigator.language, {
                 hour: '2-digit',
@@ -23,4 +21,3 @@
         document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_date').value =
             new Date().toLocaleDateString();
     }
-</script>
