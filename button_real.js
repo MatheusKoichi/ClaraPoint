9def64dac4d8720bd94a93a9aefbe5f63d6efefd
Matheus Henrique A. de Santana <53881266+MatheusKoichi@users.noreply.github.com>
2021-05-06 18:08:46 -0300

    Update button_real.js

--- button_real.js
+++ button_real.js
@@ -1,18 +1,6 @@
 <script>
 
-    if (window.location.protocol != "https:") {
-        window.location.protocol = "https:";
-        window.location.reload();
-    }
-    if (window.location.href = "https://claracloud.freshservice.com/support/catalog/items/55") {
-
-        document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick =
-            setStartTime();
-
-        document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_saida_162904').onclick =
-            setStartTime();
-
-        function setStartTime() {
+   function setStartTime() {
             document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_time').value =
                 new Date().toLocaleTimeString(navigator.language, {
                     hour: '2-digit',
@@ -23,6 +11,10 @@
             document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_entrada_162904_date').value =
                 new Date().toLocaleDateString();
         }
+
+        document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_entrada_162904').onclick = setStartTime();
+
+        document.getElementById('requested_item_values_55_requested_item_value_attributes_cf_ponto_de_saida_162904').onclick = setStartTime();
 
     }
 </script>
